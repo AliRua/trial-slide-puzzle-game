cc056afa3e62c185fa715bf29c407f1f15193225
AliRua <>
2023-07-21 15:15:49 +0000

    Add basic code for game structure and adjust style

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let rows = 3;
+let columns = 3;
+
+let currTile;
+let otherTile;
+
+let turns = 0;
+
+let imgOrder = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+window.onload = function () {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+
+            let tile = document.createElement('img');
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = imgOrder.shift() + ".jpg";
+
+            document.getElementById('board').append(tile);
+        }
+    }
+};
